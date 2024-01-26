3572d3480ffe4fb2ed459c2b15e8e599ade8ddf6
NicoVG12 <nico2001dec@gmail.com>
2024-01-26 20:05:29 -0300

    Add: Assets and MainUI

---
+++ HoraryClock/HoraryClockUI/Form1.cs
@@ -0,0 +1,27 @@
+using HoraryClockUI.Controls.MainWindow;
+
+namespace HoraryClockUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeTitle();
+
+
+
+        }
+
+        private void InitializeTitle()
+        {
+            Text = "Horary Clock Tool v2.0";
+            lblBackground.Text = Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
