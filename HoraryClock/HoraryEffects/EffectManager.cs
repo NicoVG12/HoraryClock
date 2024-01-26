a092fcba139b11aba55f4dbb4da7ebc058978a9b
NicoVG12 <nico2001dec@gmail.com>
2024-01-26 19:13:34 -0300

    Add: HoraryEffects

---
+++ HoraryClock/HoraryEffects/EffectManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoraryEffects
+{
+    public class EffectManager
+    {
+        public List<EffectType> Effects { get { return new List<EffectType>(Effects); } set { Effects = value; } }
+        public int CurrentEffectId { get; set; }
+
+        public int NextEffectId()
+        {
+            return (CurrentEffectId + 1) % Effects.Count;
+        }
+
+        public void AdvanceEffect()
+        {
+            CurrentEffectId = NextEffectId();
+        }
+
+        public EffectManager()
+        {
+            CurrentEffectId = 0;
+            Effects = new List<EffectType>();
+        }
+
+        public void InitializeDefaultData()
+        {
+
+        }
+    }
+}
