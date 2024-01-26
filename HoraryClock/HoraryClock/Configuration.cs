2ad366231989c8b07135f6ec478b5748c76b7876
NicoVG12 <nico2001dec@gmail.com>
2024-01-25 23:49:43 -0300

    Add: Configuration & KeyBinding classes, default configs

---
+++ HoraryClock/HoraryClock/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace HoraryClock
+{
+    public class Configuration
+    {
+        public int LanguageId { get; set; }
+        public bool PvPOffsett { get; set; }
+        public bool StopClockOnReset { get; set; }
+        
+        public KeyBinding KeyBindings { get; set; }
+
+        //TODO: Add SettingsReader and settings file
+        public Configuration() 
+        {
+            LanguageId = LanguageIds.ENGLISH;
+            PvPOffsett = false;
+            StopClockOnReset = false;
+            KeyBindings = new KeyBinding();
+        }
+    }
+}
