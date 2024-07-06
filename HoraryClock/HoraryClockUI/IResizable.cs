c63a8192a5916bb9b7d3115592e1ab68304ab921
NicoVG12 <nico2001dec@gmail.com>
2024-07-06 00:38:34 -0300

    Update: appconfig & IResizable imports

--- HoraryClock/HoraryClockUI/IResizable.cs
+++ HoraryClock/HoraryClockUI/IResizable.cs
@@ -1,9 +1,4 @@
-﻿using Language;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Settings;
 
 namespace HoraryClockUI
 {
